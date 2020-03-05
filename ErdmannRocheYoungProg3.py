63a675a5ca56ceec527d27f06002d7c402163848
Jude Young <jyoung006@student.franciscan.edu>
2020-03-05 18:55:44 +0000

    Started working on King class but didn't get very far...

--- ErdmannRocheYoungProg3.py
+++ ErdmannRocheYoungProg3.py
@@ -13,3 +13,19 @@ class Player(enum.Enum):
     @property
     def other(self):
         return Player.black if self == Player.red else Player.red
+
+#Added by Jude
+class King():
+    def __init__(self, color, piece, kings):
+        self.color = color
+        self.piece = piece
+        self.kings = kings
+    
+    def get_crowned_king(self, piece):
+        if self.piece is not in kings
+            kings.append(self.piece)
+            return kings
+    def add_king_moves(self, piece):
+        assert(self.piece is in kings)
+        self.add_backwards_capabilities(piece)
+    def add_backwards_capabilities(self):
