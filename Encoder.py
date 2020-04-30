672ec786fde2e5553ca34d1352c06c3708b1d27e
Jude Young <jyoung006@student.franciscan.edu>
2020-04-30 19:12:55 +0000

    Update Encoder.py

--- Encoder.py
+++ Encoder.py
@@ -1,15 +1,15 @@
 import importlib
 import numpy as np
-from checkerboard import __all__
+from checkerboard import Board, GameState, Checker, Square
 
 class Encoder:
     def name(self):
         raise NotImplementedError()
 
-    def encode(self, game_state):
+    def encode(self, GameState):
         raise NotImplementedError()
 
-    def encode_point(self, point):
+    def encode_point(self, checker):
         raise NotImplementedError()
 
     def decode_points(self):
@@ -25,44 +25,50 @@ class Encoder:
         raise NotImplementedError()
 
     def get_encoder_by_name(self, name, board_size = 8):
-        if isinstance(board_size, int):
-            board_size = (board_size, board_size)
+        board_size = (board_size, board_size)
+        module = importlib.import_module('ErdmannRocheYoungProg3.py' + name)
         constructor = getattr(module, 'create')
         return constructor(board_size)
 
-    class OnePlaneEncoder(Encoder):
-        def __init__(self, board_size = 8):
-            self.board_width, self.board_height = board_size
-            self.num_planes = 1
+class OnePlaneEncoder(Encoder):
+    def __init__(self, board_size = 8):
+        self.board_width, self.board_height = board_size
+        self.num_planes = 1
 
-        def name(self):
-            return 'oneplane'
-
-        def encode(self, game_state):
-            board_matrix = np.zeros(self.shape())
-            next_player = game_state.next_player
-            for r in range(self.board_height):
-                for c in range(self.board_width):
-                    s = Square(row = r+1, col = c+1)
-                    checker = game_state.board.get_checker(s)
-                    if checker is None:
-                        continue
-                    if checker.color == next_player:
+    def name(self):
+        return 'oneplane'
+    
+    def encode(self, GameState):
+        board_matrix = np.zeros(self.shape())
+        next_player = GameState.next_player
+        for r in range(self.board_height):
+           for c in range(self.board_width):
+                s = Square(row = r+1, col = c+1)
+                checker = GameState.board.get(s)
+                if checker is None:
+                    continue
+                if checker.color == next_player:
+                    if checker.is_king:
+                        board_matrix[0, r, c] = 2
+                    else:
                         board_matrix[0, r, c] = 1
+                else:
+                    if checker.is_king:
+                        board_matrix[0, r, c] = -2
                     else:
                         board_matrix[0, r, c] = -1
-            return board_matrix
+        return board_matrix
 
-        def encode_square(self, square):
-            return self.board_width * (square.row - 1) + (square.col - 1)
+    def encode_checker(self, checker):
+        return self.board_width * (Square.row - 1) + (Square.col - 1)
 
-        def decode_square_index(self, index):
-            row = index // self.board_width
-            col = index % self.board_width
-            return Square(row = row + 1, col = col + 1)
+    def decode_square_index(self, index):
+        row = index // 8
+        col = index % 8
+        return Square(row = row + 1, col = col + 1)
 
-        def num_squares(self):
-            return self.board_width * self.board_height
+    def num_squares(self):
+        return self.board_width * self.board_height
 
-        def shape(self):
-            return self.num_planes, self.board_height, self.board_width
+    def shape(self):
+        return self.num_planes, self.board_height, self.board_width
